f182ba9851e382f3262eb77b5c6422fbde6f82a8
williambayliss <wmbayliss@gmail.com>
2022-05-02 18:43:50 -0400

    Add test for user association

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -4,7 +4,18 @@ class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "it exists" do
+
+  test "it has content" do
     assert_equal "MyString", posts(:one).content
+  end
+
+  test "it has a user_id as foreign key" do
+    assert_equal 1, posts(:one).user_id
+  end
+
+  test "it can be built from a User model" do
+    user = User.new
+    post = user.posts.build(content: "test")
+    assert_equal user.id, post.id
   end
 end
