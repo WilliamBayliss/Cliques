a79f07db4d28378b8ccf70c29de9ed63fce8bc72
williambayliss <wmbayliss@gmail.com>
2022-05-02 19:04:43 -0400

    empty spacve

--- test/models/clique_test.rb
+++ test/models/clique_test.rb
@@ -7,4 +7,10 @@ class CliqueTest < ActiveSupport::TestCase
   test "it has a title" do
     assert_equal "title", cliques(:one).title
   end
+
+  test "it has a description" do
+    assert_equal "description", cliques(:one).description
+  end
+
+  
 end
