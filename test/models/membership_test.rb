b2cbaa265ef23d3d65af56bcf251b6d2f0ab233e
williambayliss <wmbayliss@gmail.com>
2022-05-05 12:12:30 -0400

    Initial commit

--- test/models/membership_test.rb
+++ test/models/membership_test.rb
@@ -4,17 +4,4 @@ class MembershipTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  setup do
-    @user = User.new(id: 1)
-    @clique = Clique.new(id: 1)
-    @membership = Membership.new(user_id: 1, clique_id: 1)  
-  end
-
-  test "it has a user" do
-    assert_equal @user, @membership.user
-  end
-
-  test "it has a clique" do
-    assert_equal @clique, @membership.clique
-  end
 end
