c7c6c2d0dd268452edd739dc9720a92339ac659a
williambayliss <wmbayliss@gmail.com>
2022-05-06 19:47:07 -0400

    add content test

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,7 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "it has content" do
+    assert_equal "test", comments(:one).content
+  end
 end
