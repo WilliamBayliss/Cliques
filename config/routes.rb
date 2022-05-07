a5a2696778f2754b419409594a249267d1f700ac
williambayliss <wmbayliss@gmail.com>
2022-05-06 21:05:48 -0400

    Generate timeline controller with show action, singular resource routing for main page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :comments
   resources :cliques
   resources :posts
+
+  resource :timeline, only: :show
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
