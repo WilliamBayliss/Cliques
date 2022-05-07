f7247b43fcef7adc2b08fff5617cc0f3527b4037
williambayliss <wmbayliss@gmail.com>
2022-05-06 20:23:22 -0400

    Add belongs to columns before migration

--- db/migrate/20220507002034_create_adminships.rb
+++ db/migrate/20220507002034_create_adminships.rb
@@ -1,7 +1,8 @@
 class CreateAdminships < ActiveRecord::Migration[7.0]
   def change
     create_table :adminships do |t|
-
+      t.belongs_to :user
+      t.belongs_to :clique
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_07_001851) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_07_002034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "adminships", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "clique_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["clique_id"], name: "index_adminships_on_clique_id"
+    t.index ["user_id"], name: "index_adminships_on_user_id"
+  end
 
   create_table "cliques", force: :cascade do |t|
     t.datetime "created_at", null: false
