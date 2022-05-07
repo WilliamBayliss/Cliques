3dddd617977a72bcdb38c079a06952c73ca3b5ab
williambayliss <wmbayliss@gmail.com>
2022-05-06 20:21:20 -0400

    Add admins as users rel

--- app/models/clique.rb
+++ app/models/clique.rb
@@ -1,4 +1,7 @@
 class Clique < ApplicationRecord
     has_many :memberships
     has_many :users, through: :memberships
+
+    has_many :adminships
+    has_many :administrators, through: :adminships, class_name: :user
 end
