1dd21c9f6485db3e62845e73410308b215c80b80
williambayliss <wmbayliss@gmail.com>
2022-05-06 20:32:36 -0400

    Add clique test

--- test/models/adminship_test.rb
+++ test/models/adminship_test.rb
@@ -4,4 +4,7 @@ class AdminshipTest < ActiveSupport::TestCase
   test "it has a user" do
     assert_equal users(:one), adminships(:one).user
   end
+  test "it has a clique" do
+    assert_equal cliques(:one), adminships(:one).clique 
+  end
 end
