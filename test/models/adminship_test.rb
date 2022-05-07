8964aea5627aa131c7274b30bd0a4d3b863620ec
williambayliss <wmbayliss@gmail.com>
2022-05-06 20:31:50 -0400

    Add test for user

--- test/models/adminship_test.rb
+++ test/models/adminship_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class AdminshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "it has a user" do
+    assert_equal users(:one), adminships(:one).user
+  end
 end
