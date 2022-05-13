da526b54d6e406e5f440f035b479647db483a670
williambayliss <wmbayliss@gmail.com>
2022-05-12 23:49:36 -0400

    add devise integration test helpers to handle user auth before controller actions

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
   setup do
     @comment = comments(:one)
+    sign_in users(:one)
   end
 
   test "should get index" do
