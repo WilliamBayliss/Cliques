b1082a58231d991f3a20e9ba119e20234b182dab
williambayliss <wmbayliss@gmail.com>
2022-05-17 02:10:56 -0400

    set root, add modified routes for devise users

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   resources :comments
   resources :cliques
   resources :posts
-
-  resource :timeline, only: :show
-  root "timelines#show"
+  root "posts#index"
   devise_for :users
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
