d4413ceb3dda872fef8b5f8ff91a566b1db5a2c5
williambayliss <wmbayliss@gmail.com>
2022-05-17 12:11:47 -0400

    Add titles to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_13_040533) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_161126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_13_040533) do
     t.integer "score"
     t.string "commentable_type"
     t.bigint "commentable_id"
+    t.string "title"
     t.index ["commentable_type", "commentable_id"], name: "index_posts_on_commentable"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
