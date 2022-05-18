69b34a6185fdaf0ccd573019975b24f7ddbdd8df
williambayliss <wmbayliss@gmail.com>
2022-05-18 00:34:43 -0400

    remove user bios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_034846) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_043343) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,7 +76,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_18_034846) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.date "birthday"
-    t.string "bio"
     t.integer "reputation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
