9b3045848ee3e510cfafdb3349cf40d3542f9034
williambayliss <wmbayliss@gmail.com>
2022-05-19 11:40:48 -0400

    add gray colours to theme

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,7 +11,20 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-    },
+      colors: {
+        gray: {
+          '100': '#F8F8F8',
+          '200': '#E0E0E0',
+          '300': '#C8C8C8',
+          '400': '#888888',
+          '500': '#707070',
+          '600': '#505050',
+          '700': '#383838',
+          '800': '#282828',
+          '900': '#101010',
+        }
+      }
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
