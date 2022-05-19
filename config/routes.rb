df2b8cb28c52d94a5c4dbf0291b00e08cab7f484
williambayliss <wmbayliss@gmail.com>
2022-05-19 12:30:46 -0400

    add resources users, change devise for users paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   resources :comments
   resources :cliques
   resources :posts
+  resources :users, only: :show
   root "posts#index"
-  devise_for :users
-  devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
-    get 'sign_up', to: 'devise/registrations#new'
-  end
+  devise_for :users, path: '', path_names: { sign_in: 'sign_in', sign_out: 'sign_out', sign_up: 'sign_up' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
