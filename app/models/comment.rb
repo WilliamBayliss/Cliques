c2f327249bd4fcff0185a4e485762de8a1cd6c27
williambayliss <wmbayliss@gmail.com>
2022-05-23 13:11:11 -0400

    add acts as votable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :commentable, polymorphic: true
     has_many :comments, as: :commentable
-
+    acts_as_votable
     def add_score
         self.post.score += 1
         self.post.save
