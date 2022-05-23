a53b500e71c0763431ad80cd8c135b06f774540b
williambayliss <wmbayliss@gmail.com>
2022-05-23 00:08:07 -0400

    add acts as votable to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
-    validates :content, presence: true
-    belongs_to :user
-    belongs_to :clique
-    has_many :comments, as: :commentable
+  acts_as_votable
+  validates :content, presence: true
+  belongs_to :user
+  belongs_to :clique
+  has_many :comments, as: :commentable
 end
