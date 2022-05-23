ed2b91d921621383935eaedb81cc94526577009f
williambayliss <wmbayliss@gmail.com>
2022-05-23 13:48:08 -0400

    add routes for upvote and downvote methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :comments
   resources :cliques
-  resources :posts
+  resources :posts do
+    post 'upvote' => 'posts#upvote', as: :upvote
+    post 'downvote' => 'posts#downvote', as: :downvote
+  end
   resources :users, only: :show
   root "posts#index"
   devise_for :users, path: '', path_names: { sign_in: 'sign_in', sign_out: 'sign_out', sign_up: 'sign_up' }
