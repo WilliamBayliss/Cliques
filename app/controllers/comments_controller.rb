de37ef7d5c10a2255447c50554d874994b200939
williambayliss <wmbayliss@gmail.com>
2022-06-05 15:16:40 -0400

    Add upvote and downvote methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -58,6 +58,22 @@ class CommentsController < ApplicationController
     end
   end
 
+  def upvote
+    @comment = Comment.find(params[:comment_id])
+    @comment.upvote_from current_user
+    respond_to do |format|
+      format.html { redirect_to comment_url(@comment) }
+    end
+  end
+  
+  def downvote
+    @comment = Comment.find(params[:comment_id])
+    @comment.downvote_from current_user
+    respond_to do |format|
+      format.html { redirect_to comment_url(@comment) }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
