fd22f92f0ab4c33ef6c437d4d8109fb4c9aaaf2b
williambayliss <wmbayliss@gmail.com>
2022-06-09 13:23:12 -0400

    Remove score methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,13 +3,4 @@ class Comment < ApplicationRecord
     belongs_to :commentable, polymorphic: true
     has_many :comments, as: :commentable
     acts_as_votable
-    def add_score
-        self.post.score += 1
-        self.post.save
-    end
-
-    def subtract_score
-        self.post.score -= 1
-        self.post.save
-    end
 end
