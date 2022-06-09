d2d2477cd8f0f654295945fc8359d29b84ea2c81
williambayliss <wmbayliss@gmail.com>
2022-06-09 13:53:17 -0400

    add method to update reputation

--- app/models/clique.rb
+++ app/models/clique.rb
@@ -8,4 +8,14 @@ class Clique < ApplicationRecord
     has_many :posts, dependent: :destroy
 
     validates :title, format: { without: /\s/, message: "Title cannot contain spaces." }
+
+
+  def update_reputation
+    reputation = 0
+    self.posts.each do |post|
+      reputation += post.cached_votes_score
+    end
+    self.reputation = reputation
+    self.save
+  end
 end
