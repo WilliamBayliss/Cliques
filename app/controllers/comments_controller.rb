bc56c503de9f782d414523599128debbc7644410
williambayliss <wmbayliss@gmail.com>
2022-06-15 12:31:34 -0400

    Change to respond to turbo stream

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,7 +14,6 @@ class CommentsController < ApplicationController
   # GET /comments/new
   def new
     @comment = Comment.new
-    @comment.post_id = @post.id
   end
 
   # GET /comments/1/edit
@@ -24,11 +23,11 @@ class CommentsController < ApplicationController
   # POST /comments or /comments.json
   def create
     @comment = current_user.comments.build(comment_params)
-    @comment.post_id = @post.id
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
-        format.json { render :show, status: :created, location: @comment }
+        format.turbo_stream
+        # format.html { redirect_to comment_url(@comment), notice: "Comment was successfully created." }
+        # format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
