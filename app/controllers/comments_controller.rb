08cf8a1f5484426df8b0a714bb54fb62ee1911c0
williambayliss <wmbayliss@gmail.com>
2022-06-15 16:22:41 -0400

    add commentable to params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -86,6 +86,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:content, :commentable_type, :commentable_id)
+      params.require(:comment).permit(:content, :commentable_id, :commentable_type)
     end
 end
