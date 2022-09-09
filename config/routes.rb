85997f56241c2ba911680b75eac939a770b0498c
William Bayliss <wmbayliss@gmail.com>
2022-09-09 14:24:16 -0400

    remove index from users routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show]
   root "posts#index"
   devise_for :users, path: '', path_names: { sign_in: 'sign_in', sign_out: 'sign_out', sign_up: 'sign_up' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
