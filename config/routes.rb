1f20b4017fec03ca1d98e0426eabf2591e2655f0
William Bayliss <wmbayliss@gmail.com>
2022-09-16 15:02:15 -0400

    remove autogen comments

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:show]
+  
   root "posts#index"
   devise_for :users, path: '', path_names: { sign_in: 'sign_in', sign_out: 'sign_out', sign_up: 'sign_up' }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
