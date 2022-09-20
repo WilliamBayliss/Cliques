44aa2bc13f07624070f4537037565c844cc73f35
William Bayliss <wmbayliss@gmail.com>
2022-09-20 13:37:19 -0400

    Add stimulus controller for dropdown menus

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "menu" ]
+  connect() {
+    console.log()
+  }
+
+  toggle() {
+    if (this.menuTarget.classList.contains("hidden")) {
+        this.menuTarget.classList.remove("hidden");
+    } else {
+        this.menuTarget.classList.add("hidden")
+    }
+  }
+}
